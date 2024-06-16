afb8697c0ca9a00866e5c87cd27b92205e8f29fd
Amir A <amiragrama@gmail.com>
2024-06-15 21:25:31 -0400

    initial commit

---
+++ devenv.nix
@@ -0,0 +1,21 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = with pkgs; [ git ];
+
+  languages.javascript.enable = true;
+  services.postgres = {
+    enable = true;
+    listen_addresses = "0.0.0.0";
+    initialScript = "CREATE ROLE postgres SUPERUSER; ALTER ROLE postgres WITH LOGIN;";
+    initialDatabases = [{ name = "dbthing"; }];
+  };
+ 
+  enterShell = ''
+  '';
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+  '';
+}
